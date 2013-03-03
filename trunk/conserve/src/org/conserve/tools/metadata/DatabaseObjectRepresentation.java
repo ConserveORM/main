9efb4076d9068b8e280cc3e824c042012985c1c3
erik.berglund@gmail.com <erik.berglund@gmail.com@40553850-3f96-7157-1f63-669acec06908>
2013-03-03 12:10:36 +0000

    Implemented loading of metadata from database.

--- trunk/conserve/src/org/conserve/tools/metadata/DatabaseObjectRepresentation.java
+++ trunk/conserve/src/org/conserve/tools/metadata/DatabaseObjectRepresentation.java
@@ -1,8 +1,14 @@
 package org.conserve.tools.metadata;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.conserve.adapter.AdapterBase;
 import org.conserve.connection.ConnectionWrapper;
+import org.conserve.tools.Defaults;
 import org.conserve.tools.NameGenerator;
+import org.conserve.tools.ObjectTools;
 
 /**
  * Metadata about an object loaded from the database.
@@ -17,8 +23,10 @@ public class DatabaseObjectRepresentation extends ObjectRepresentation
 	 * @param adapter 
 	 * @param c the class to load.
 	 * @param cw
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
 	 */
-	public DatabaseObjectRepresentation(AdapterBase adapter, Class<?> c, ConnectionWrapper cw)
+	public DatabaseObjectRepresentation(AdapterBase adapter, Class<?> c, ConnectionWrapper cw) throws SQLException, ClassNotFoundException
 	{
 		this.clazz = c;
 		this.adapter = adapter;
@@ -29,11 +37,26 @@ public class DatabaseObjectRepresentation extends ObjectRepresentation
 	/**
 	 * Load the class description from the database.
 	 * @param cw
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
 	 */
-	private void doLoad(ConnectionWrapper cw)
+	private void doLoad(ConnectionWrapper cw) throws SQLException, ClassNotFoundException
 	{
-		// TODO Auto-generated method stub
-		
+		//get the column names and types from the database
+		StringBuilder stmt = new StringBuilder("SELECT COLUMN_NAME,COLUMN_CLASS FROM ");
+		stmt.append(Defaults.TYPE_TABLENAME);
+		stmt.append(" WHERE OWNER_TABLE = ?");
+		PreparedStatement ps = cw.prepareStatement(stmt.toString());
+		ps.setString(1, getTableName());
+		ResultSet rs = ps.executeQuery();
+		while(rs.next())
+		{
+			String name = rs.getString(1);
+			String classDesc = rs.getString(2);
+			Class<?> c = ObjectTools.lookUpClass(classDesc);
+			this.returnTypes.add(c);
+			this.props.add(name);
+		}
 	}
 	
 }

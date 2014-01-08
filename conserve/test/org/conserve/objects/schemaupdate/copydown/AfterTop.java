3e687c4a421877504ac67cb33a0771f17b1080d8
erik.berglund@gmail.com <erik.berglund@gmail.com@40553850-3f96-7157-1f63-669acec06908>
2014-01-08 12:05:55 +0000

    All tests for protection entry now implemented. Three tests currently failing.

---
+++ conserve/test/org/conserve/objects/schemaupdate/copydown/AfterTop.java
@@ -0,0 +1,27 @@
+package org.conserve.objects.schemaupdate.copydown;
+
+/**
+ * @author Erik Berglund
+ *
+ */
+public class AfterTop
+{
+	private Object foo;
+
+	/**
+	 * @return the foo
+	 */
+	public Object getFoo()
+	{
+		return foo;
+	}
+
+	/**
+	 * @param foo the foo to set
+	 */
+	public void setFoo(Object foo)
+	{
+		this.foo = foo;
+	}
+
+}

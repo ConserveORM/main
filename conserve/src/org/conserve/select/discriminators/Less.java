f17fd3475e3c7abac7fa96a9443746d2ef768039
Erik Berglund <erik.berglund@hiq.se>
2016-05-26 15:32:42 +0200

    Switched license from LGPL to AGPL. Updated copyright notices accordingly.

--- conserve/src/org/conserve/select/discriminators/Less.java
+++ conserve/src/org/conserve/select/discriminators/Less.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2013 Erik Berglund.
- *   
- *      This file is part of Conserve.
- *   
- *       Conserve is free software: you can redistribute it and/or modify
- *       it under the terms of the GNU Lesser General Public License as published by
- *       the Free Software Foundation, either version 3 of the License, or
- *       (at your option) any later version.
- *   
- *       Conserve is distributed in the hope that it will be useful,
- *       but WITHOUT ANY WARRANTY; without even the implied warranty of
- *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *       GNU Lesser General Public License for more details.
- *   
- *       You should have received a copy of the GNU Lesser General Public License
- *       along with Conserve.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (c) 2009, 2016 Erik Berglund.
+ *    
+ *        This file is part of Conserve.
+ *    
+ *        Conserve is free software: you can redistribute it and/or modify
+ *        it under the terms of the GNU Affero General Public License as published by
+ *        the Free Software Foundation, either version 3 of the License, or
+ *        (at your option) any later version.
+ *    
+ *        Conserve is distributed in the hope that it will be useful,
+ *        but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *        GNU Affero General Public License for more details.
+ *    
+ *        You should have received a copy of the GNU Affero General Public License
+ *        along with Conserve.  If not, see <https://www.gnu.org/licenses/agpl.html>.
  *******************************************************************************/
 package org.conserve.select.discriminators;
 

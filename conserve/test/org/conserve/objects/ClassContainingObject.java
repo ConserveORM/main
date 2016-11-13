bbd4987f8b528dc561fe6bc2aaa5d1cf69374568
Erik Berglund <erik.berglund@gmail.com>
2016-11-13 10:13:20 +0100

    Creating a new branch with experimental queries optimised for size.

---
+++ conserve/test/org/conserve/objects/ClassContainingObject.java
@@ -0,0 +1,32 @@
+package org.conserve.objects;
+
+/**
+ * @author Erik Berglund
+ *
+ */
+public class ClassContainingObject
+{
+	private Class<?> myClass;
+	private Float myValue;
+
+	public Class<?> getMyClass()
+	{
+		return myClass;
+	}
+
+	public void setMyClass(Class<?> myClass)
+	{
+		this.myClass = myClass;
+	}
+
+	public Float getMyValue()
+	{
+		return myValue;
+	}
+
+	public void setMyValue(Float myValue)
+	{
+		this.myValue = myValue;
+	}
+	
+}

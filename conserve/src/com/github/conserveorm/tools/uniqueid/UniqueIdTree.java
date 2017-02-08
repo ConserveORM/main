21fa12713378064ba7161bfb90130f8ab11a2f46
Erik Berglund <erik.berglund@gmail.com>
2017-02-08 20:11:34 +0100

    Updated copyright notice.

--- conserve/src/com/github/conserveorm/tools/uniqueid/UniqueIdTree.java
+++ conserve/src/com/github/conserveorm/tools/uniqueid/UniqueIdTree.java
@@ -1,20 +1,22 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2016 Erik Berglund.
+ *  
+ * Copyright (c) 2009, 2017 Erik Berglund.
  *    
- *        This file is part of Conserve.
- *    
- *        Conserve is free software: you can redistribute it and/or modify
- *        it under the terms of the GNU Affero General Public License as published by
- *        the Free Software Foundation, either version 3 of the License, or
- *        (at your option) any later version.
- *    
- *        Conserve is distributed in the hope that it will be useful,
- *        but WITHOUT ANY WARRANTY; without even the implied warranty of
- *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *        GNU Affero General Public License for more details.
- *    
- *        You should have received a copy of the GNU Affero General Public License
- *        along with Conserve.  If not, see <https://www.gnu.org/licenses/agpl.html>.
+ *       This file is part of Conserve.
+ *   
+ *       Conserve is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU Affero General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *   
+ *       Conserve is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU Affero General Public License for more details.
+ *   
+ *       You should have received a copy of the GNU Affero General Public License
+ *       along with Conserve.  If not, see <https://www.gnu.org/licenses/agpl.html>.
+ *       
  *******************************************************************************/
 package com.github.conserveorm.tools.uniqueid;
 
